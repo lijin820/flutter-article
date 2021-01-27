b8708b70a025e45aca0ea2ba72f893dc4a6853d8
Li Jin <lijin1991.dev@gmail.com>
2021-01-27 16:45:28 +0800

    customized bottom tab navigation

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -13,18 +13,27 @@ class _HomeState extends State<Home> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _children = <Widget>[
     Text(
-      'Index 0: Home',
+      'Index 0: The Latest',
       style: optionStyle,
     ),
     Text(
-      'Index 1: Business',
+      'Index 1: Learning',
       style: optionStyle,
     ),
     Text(
-      'Index 2: School',
+      'Index 2: Resources',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Discussions',
       style: optionStyle,
     ),
   ];
+  final tabIconSize = 24.0;
 
   void onTabTapped(int index) {
     setState(() {
@@ -36,7 +45,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Flutter App'),
+        title: Text('Article'),
       ),
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -44,14 +53,25 @@ class _HomeState extends State<Home> {
         currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            label: 'Home',
-          ),
+              icon: new Icon(Icons.home),
+              label: 'Latest',
+              backgroundColor: Colors.lightGreen),
           BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            label: 'Messages',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+              icon: new Icon(Icons.school),
+              label: 'Learning',
+              backgroundColor: Colors.lightGreen),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.camera_alt),
+              label: 'Resources',
+              backgroundColor: Colors.lightGreen),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Colors.lightGreen),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.commute),
+              label: 'Discussions',
+              backgroundColor: Colors.lightGreen),
         ],
       ),
     );
