94eb8d5352e07b4aad6dc75118e02285301eb377
Li Jin <lijin1991.dev@gmail.com>
2021-01-29 11:55:01 +0800

    added indexed router

--- lib/utils/destination.dart
+++ lib/utils/destination.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Destination {
-  const Destination(this.title, this.icon, this.color);
+  const Destination(this.index, this.title, this.icon, this.color);
+  final int index;
   final String title;
   final IconData icon;
   final MaterialColor color;
 }
 
 const List<Destination> allDestinations = <Destination>[
-  Destination('Latest', Icons.home, Colors.teal),
-  Destination('Learning', Icons.school, Colors.teal),
-  Destination('Resources', Icons.camera_alt, Colors.teal),
-  Destination('Search', Icons.search, Colors.teal),
-  Destination('Discussions', Icons.commute, Colors.teal)
+  Destination(0, 'Latest', Icons.home, Colors.teal),
+  Destination(1, 'Learning', Icons.school, Colors.teal),
+  Destination(2, 'Resources', Icons.camera_alt, Colors.teal),
+  Destination(3, 'Search', Icons.search, Colors.teal),
+  Destination(4, 'Discussions', Icons.commute, Colors.teal)
 ];
--- lib/widgets/destination_widget.dart
+++ lib/widgets/destination_widget.dart
@@ -4,10 +4,26 @@ import 'package:flutter_article/widgets/list_page_widget.dart';
 import 'package:flutter_article/widgets/root_page_widget.dart';
 import 'package:flutter_article/widgets/text_page_widget.dart';
 
+class ViewNavigatorObserver extends NavigatorObserver {
+  ViewNavigatorObserver(this.onNavigation);
+
+  final VoidCallback onNavigation;
+
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
+  }
+
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
+  }
+}
+
 class DestinationView extends StatefulWidget {
-  const DestinationView({Key key, this.destination}) : super(key: key);
+  const DestinationView({Key key, this.destination, this.onNavigation})
+      : super(key: key);
 
   final Destination destination;
+  final VoidCallback onNavigation;
 
   @override
   _DestinationViewState createState() => _DestinationViewState();
@@ -27,10 +43,12 @@ class _DestinationViewState extends State<DestinationView> {
   @override
   Widget build(BuildContext context) {
     return Navigator(
+      observers: <NavigatorObserver>[
+        ViewNavigatorObserver(widget.onNavigation),
+      ],
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute(
           settings: settings,
-          // ignore: missing_return
           builder: (BuildContext context) {
             switch (settings.name) {
               case '/':
@@ -39,16 +57,12 @@ class _DestinationViewState extends State<DestinationView> {
                 return ListPage(destination: widget.destination);
               case '/text':
                 return TextPage(destination: widget.destination);
+              default:
+                return RootPage(destination: widget.destination);
             }
           },
         );
       },
     );
-  }
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
   }
 }
